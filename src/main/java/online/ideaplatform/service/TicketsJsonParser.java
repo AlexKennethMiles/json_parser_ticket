f70f75fdf4037eba6756d983ea467883d1e83e00
AlexKennethMiles <apuchkov17@gmail.com>
2024-01-27 22:32:53 +0300

    4. Исправления и настройка для сборки jar

--- src/main/java/online/ideaplatform/service/TicketsJsonParser.java
+++ src/main/java/online/ideaplatform/service/TicketsJsonParser.java
@@ -3,14 +3,17 @@ package online.ideaplatform.service;
 import com.google.gson.Gson;
 import online.ideaplatform.model.Root;
 
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class TicketsJsonParser {
     public static Root parse() {
         Gson gson = new Gson();
-
-        try (FileReader reader = new FileReader("files/tickets.json")) {
+        try (InputStreamReader reader = new InputStreamReader(
+                TicketsJsonParser.class.getResourceAsStream("/tickets.json"),
+                StandardCharsets.UTF_8
+        )) {
             return gson.fromJson(reader, Root.class);
         } catch (IOException e) {
             System.out.println("\u001B[31m" + "Ошибка чтения файла " + e + "\u001B[0m");
--- src/main/java/online/ideaplatform/service/TimeDeserializer.java
+++ src/main/java/online/ideaplatform/service/TimeDeserializer.java
@@ -8,20 +8,12 @@ import com.google.gson.JsonParseException;
 import java.lang.reflect.Type;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class TimeDeserializer implements JsonDeserializer<LocalTime> {
-    private final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm");
-    private final DateTimeFormatter altTimeFormat = DateTimeFormatter.ofPattern("H:mm");
+    private final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("H:mm");
 
     @Override
-    public LocalTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
-        try {
-            return LocalTime.parse(json.getAsString(), timeFormat);
-        } catch (DateTimeParseException e) {
-            return LocalTime.parse(json.getAsString(), altTimeFormat);
-        } catch (Exception e) {
-            throw new JsonParseException(e);
-        }
+    public LocalTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        return LocalTime.parse(json.getAsString(), timeFormat);
     }
 }
